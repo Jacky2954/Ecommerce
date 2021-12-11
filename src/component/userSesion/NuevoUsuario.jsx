74bf3484262a0b800edec8436734657604384ac3
Jacky2954 <jackelineulloabarzola@gmail.com>
2021-12-11 17:56:40 -0500

    commit 14
    Construyendo Login y Registro parte 2

--- src/component/userSesion/NuevoUsuario.jsx
+++ src/component/userSesion/NuevoUsuario.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, Form, Card} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Register extends React.Component {
@@ -16,22 +16,34 @@ class Register extends React.Component {
     render (){
         return (
             <div>
-                <Button className="btn btn-light ms-2" onClick={()=>this.handleModal()}>
+                <Button variant="btn btn-outline-light ms-2" onClick={()=>this.handleModal()}>
                 <i className="fa fa-user-plus me-1"></i>Regístrate</Button>
 
                 <Modal show={this.state.showModal} onHide={()=>this.handleModal()}>
                     <Modal.Header closeButton>
-                        hola 
+                        <Card.Title>
+                        Regístrate y conviértete en un Nowlover
+                        </Card.Title>
                     </Modal.Header>
-
                     <Modal.Body>
-                        hola 
-                    </Modal.Body>
+                        <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Usuario</Form.Label>
+                            <Form.Control type="email" />
+                        </Form.Group>
 
-                    <Modal.Footer>
-                        <Button>Abrir</Button>
-                        <Button onClick={()=>this.handleModal()}>Cerrar</Button>
-                    </Modal.Footer>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={()=>this.handleModal()}>
+                            Submit
+                        </Button>
+                        </Form>
+                    </Modal.Body>     
                 </Modal>
             </div>
         )
