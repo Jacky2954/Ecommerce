653eb708c202dee29a9f56664095b54dfcef6878
Jacky2954 <jackelineulloabarzola@gmail.com>
2021-12-11 10:56:31 -0500

    commit 12
    construyendo login opción 1 - Modal

--- src/component/userSesion/Login.jsx
+++ src/component/userSesion/Login.jsx
@@ -1,44 +1,74 @@
 import React from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Login = () => {
-    return (
-        <>
-            <button type="button" className="btn btn-outline-light ms-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-               <span className="fa fa-sign-in me-1"></span> Login
-            </button>
+class App extends React.Component {
+    state={ 
+        abierto: false,
+    }
 
-        
-            <div className="modal fade" id="loginModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Login</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <form>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>    
-  </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-                                        <div className="mb-3 form-check">
-                                            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                                                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-                                            <button type="submit" className="btn btn-outline-primary w-100 mt-5">Submit</button>
-</form>
-                                    </div>
-                                </div>
-                </div>
-                        </div>
-        </>
-                    )
+    abrirModal=()=>{
+        this.setState({abierto: !this.state.abierto})
+    }
+
+    render() {
+
+        const modalStyles={
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-56%, -56%)',
+        }
+
+        return (
+            <>
+            <Button
+                className="btn btn-outline-light ms-2"
+                onClick={this.abrirModal}>
+                    <i className="fa fa-sign-in me-1">
+                    </i>Mostrar</Button>
+            
+            
+            <Modal isOpen={this.state.abierto} style={modalStyles}>
+                <ModalHeader>
+                    Iniciar Sesión
+                </ModalHeader>
+
+                <ModalBody>
+                    <FormGroup>
+                        <Label for="usuario">Usuario</Label>
+                        <Input type="text" id="usuario" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="password">Contraseña</Label>
+                        <Input type="text" id="password" />
+                    </FormGroup>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button
+                        color="primary"
+                    >Ingresar
+                    </Button>
+
+                    <Button
+                        color="secondary"
+                        onClick={this.abrirModal}
+                    >Cancelar
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            </>
+        )
+    }
 }
 
-                    export default Login
+
+
+
+
+export default App;
+
 
   
